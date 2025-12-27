9eab48acedbafd2a94f1ba514ce29ca6c1d73ba2
maringelix <maringelix@users.noreply.github.com>
2025-12-27 17:12:18 -0300

    fix: format Terraform files with terraform fmt
    Fixed formatting issues in:
    - terraform/environments/dev/main.tf
    - terraform/environments/dev/terraform.tfvars
    
    Run: terraform fmt -recursive

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -27,11 +27,11 @@ provider "google" {
 module "networking" {
   source = "../../modules/networking"
 
-  project_id       = var.project_id
-  region           = var.region
-  network_name     = var.network_name
-  gke_subnet_cidr  = "10.0.0.0/24"
-  gke_pods_cidr    = "10.1.0.0/16"
+  project_id        = var.project_id
+  region            = var.region
+  network_name      = var.network_name
+  gke_subnet_cidr   = "10.0.0.0/24"
+  gke_pods_cidr     = "10.1.0.0/16"
   gke_services_cidr = "10.2.0.0/16"
 }
 
@@ -39,14 +39,14 @@ module "networking" {
 module "gke" {
   source = "../../modules/gke"
 
-  project_id           = var.project_id
-  region               = var.region
-  cluster_name         = var.cluster_name
-  environment          = var.environment
-  network_self_link    = module.networking.network_self_link
-  subnet_self_link     = module.networking.gke_subnet_self_link
-  pods_range_name      = module.networking.gke_pods_range_name
-  services_range_name  = module.networking.gke_services_range_name
+  project_id          = var.project_id
+  region              = var.region
+  cluster_name        = var.cluster_name
+  environment         = var.environment
+  network_self_link   = module.networking.network_self_link
+  subnet_self_link    = module.networking.gke_subnet_self_link
+  pods_range_name     = module.networking.gke_pods_range_name
+  services_range_name = module.networking.gke_services_range_name
 
   depends_on = [module.networking]
 }
@@ -55,16 +55,16 @@ module "gke" {
 module "cloudsql" {
   source = "../../modules/cloudsql"
 
-  project_id                 = var.project_id
-  region                     = var.region
-  instance_name              = var.database_instance_name
-  database_version           = "POSTGRES_16"
-  tier                       = "db-f1-micro"
-  disk_size                  = 10
-  database_name              = "dx03"
-  database_user              = "dx03"
-  network_self_link          = module.networking.network_self_link
-  private_vpc_connection_id  = module.networking.private_vpc_connection_id
+  project_id                = var.project_id
+  region                    = var.region
+  instance_name             = var.database_instance_name
+  database_version          = "POSTGRES_16"
+  tier                      = "db-f1-micro"
+  disk_size                 = 10
+  database_name             = "dx03"
+  database_user             = "dx03"
+  network_self_link         = module.networking.network_self_link
+  private_vpc_connection_id = module.networking.private_vpc_connection_id
 
   depends_on = [module.networking]
 }
@@ -85,11 +85,11 @@ module "artifact_registry" {
 module "cloud_armor" {
   source = "../../modules/cloud-armor"
 
-  project_id                  = var.project_id
-  policy_name                 = "tx03-waf-policy"
-  project_name                = "tx03"
-  enable_rate_limiting        = true
-  rate_limit_threshold        = 100
-  ban_duration_sec            = 600
-  enable_adaptive_protection  = true
+  project_id                 = var.project_id
+  policy_name                = "tx03-waf-policy"
+  project_name               = "tx03"
+  enable_rate_limiting       = true
+  rate_limit_threshold       = 100
+  ban_duration_sec           = 600
+  enable_adaptive_protection = true
 }
