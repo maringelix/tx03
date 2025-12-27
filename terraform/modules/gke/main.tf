1aa15bc7fca0c6663bd88df990933c13a50ab0bf
maringelix <maringelix@users.noreply.github.com>
2025-12-27 20:24:50 -0300

    fix: drastically simplify GKE Autopilot config
    Removed configurations causing Terraform timeout:
    - private_cluster_config (Autopilot manages this)
    - master_authorized_networks_config (not needed for Autopilot)
    - workload_identity_config (Autopilot enables by default)
    - maintenance_policy (Autopilot manages automatically)
    - addons_config (Autopilot manages automatically)
    - logging_config (Autopilot enables by default)
    - monitoring_config (Autopilot enables by default)
    
    Kept only essential configs:
    - enable_autopilot = true
    - network and subnetwork
    - ip_allocation_policy
    - release_channel
    - labels
    
    GKE Autopilot is FULLY MANAGED - less config = faster + more reliable

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -16,58 +16,9 @@ resource "google_container_cluster" "primary" {
     services_secondary_range_name = var.services_range_name
   }
 
-  # Private cluster configuration
-  private_cluster_config {
-    enable_private_nodes    = true
-    enable_private_endpoint = false
-    master_ipv4_cidr_block  = "172.16.0.0/28"
-  }
-
-  # Master authorized networks (allow GitHub Actions and your IP)
-  master_authorized_networks_config {
-    cidr_blocks {
-      cidr_block   = "0.0.0.0/0"
-      display_name = "All"
-    }
-  }
-
-  # Workload Identity
-  workload_identity_config {
-    workload_pool = "${var.project_id}.svc.id.goog"
-  }
-
   # Release channel
   release_channel {
     channel = "REGULAR"
-  }
-
-  # Maintenance window
-  maintenance_policy {
-    daily_maintenance_window {
-      start_time = "03:00"
-    }
-  }
-
-  # Addons (Autopilot manages network policies automatically)
-  addons_config {
-    http_load_balancing {
-      disabled = false
-    }
-    horizontal_pod_autoscaling {
-      disabled = false
-    }
-  }
-
-  # Logging and monitoring
-  logging_config {
-    enable_components = ["SYSTEM_COMPONENTS", "WORKLOADS"]
-  }
-
-  monitoring_config {
-    enable_components = ["SYSTEM_COMPONENTS"]
-    managed_prometheus {
-      enabled = true
-    }
   }
 
   # Resource labels
